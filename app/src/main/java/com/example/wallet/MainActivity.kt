a09f4760e7063f47c842ca347343a2e783a3c7a0
Sychao <sychaonn@gmail.com>
2023-01-27 08:04:09 +0700

    Add Scroll to top option

--- app/src/main/java/com/example/wallet/MainActivity.kt
+++ app/src/main/java/com/example/wallet/MainActivity.kt
@@ -4,9 +4,12 @@ package com.example.wallet
 import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils.replace
 import android.view.View
 import android.widget.AdapterView
+import androidx.core.widget.NestedScrollView
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
 import com.example.wallet.model.Notification
 import com.example.wallet.model.Transaction
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -28,6 +31,33 @@ class MainActivity : AppCompatActivity() {
         setCurrentFragment(firstFragment)
 
         val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottomNavigationView)
+        bottomNavigationView.setOnItemReselectedListener { item ->
+            when (item.itemId) {
+//                R.id.homeFragment -> firstFragment.view?.findViewById<RecyclerView>(R.id.upcoming_payment_View)?.scrollToPosition(0)
+//                R.id.chartFragment -> thirdFragment.view?.findViewById<RecyclerView>(R.id.upcoming_payment_chart_View)?.scrollToPosition(0)
+//                R.id.notificationFragment -> forthFragment.view?.findViewById<RecyclerView>(R.id.notification_View)?.scrollToPosition(0)
+                R.id.homeFragment -> {
+                if(firstFragment.isAdded() && firstFragment.isVisible()){
+                    val nestedScrollView = firstFragment.view?.findViewById<NestedScrollView>(R.id.nestedScroll)
+                    val recyclerView = firstFragment.view?.findViewById<RecyclerView>(R.id.upcoming_payment_View)
+                    nestedScrollView?.postDelayed( { nestedScrollView.smoothScrollTo(0, 0) }, 100)
+                }
+            }
+            R.id.chartFragment -> {
+            if(thirdFragment.isAdded() && thirdFragment.isVisible()){
+                val nestedScrollView = thirdFragment.view?.findViewById<NestedScrollView>(R.id.chart_nestedScroll)
+                val recyclerView = thirdFragment.view?.findViewById<RecyclerView>(R.id.upcoming_payment_chart_View)
+                nestedScrollView?.postDelayed({ nestedScrollView.smoothScrollTo(0, 0) }, 100)
+            }
+        }
+                R.id.notificationFragment -> {
+                    if(forthFragment.isAdded() && forthFragment.isVisible()){
+                        val recyclerView = forthFragment.view?.findViewById<RecyclerView>(R.id.notification_View)
+                        recyclerView?.post { recyclerView.scrollToPosition(0) }
+                    }
+               }
+            }
+        }
         bottomNavigationView.setOnItemSelectedListener {
             when(it.itemId){
                 R.id.homeFragment->setCurrentFragment(firstFragment)
@@ -39,20 +69,20 @@ class MainActivity : AppCompatActivity() {
         }
 
         //add test notification
-        val notifications = arrayListOf<Notification>()
-        notifications.add(Notification("Transaction Success","https://media.discordapp.net/attachments/787991185427922945/1068084096758198382/tick.png" , "We have successfully received your payment for ..."))
-        notifications.add(Notification("Transaction Failed", "https://media.discordapp.net/attachments/787991185427922945/1068083396544311306/close.png","We cannot proceed your transaction now. Please try again later."))
-        notifications.add(Notification("Transaction Report", "https://media.discordapp.net/attachments/787991185427922945/1068083396863070248/history.png","You can see your transaction history in this app."))
-        notifications.add(Notification("Transaction Success","https://media.discordapp.net/attachments/787991185427922945/1068084096758198382/tick.png" , "We have successfully received your payment for ..."))
-        notifications.add(Notification("Transaction Failed", "https://media.discordapp.net/attachments/787991185427922945/1068083396544311306/close.png","We cannot proceed your transaction now. Please try again later."))
-        notifications.add(Notification("Transaction Report", "https://media.discordapp.net/attachments/787991185427922945/1068083396863070248/history.png","You can see your transaction history in this app."))
-        val gson = Gson()
-        val json = gson.toJson(notifications)
-        val sharedPref = getSharedPreferences("notifications_list", Context.MODE_PRIVATE)
-        val editor = sharedPref.edit()
-        editor.clear()
-        editor.putString("notifications", json)
-        editor.apply()
+//        val notifications = arrayListOf<Notification>()
+//        notifications.add(Notification("Transaction Success","https://media.discordapp.net/attachments/787991185427922945/1068084096758198382/tick.png" , "We have successfully received your payment for ..."))
+//        notifications.add(Notification("Transaction Failed", "https://media.discordapp.net/attachments/787991185427922945/1068083396544311306/close.png","We cannot proceed your transaction now. Please try again later."))
+//        notifications.add(Notification("Transaction Report", "https://media.discordapp.net/attachments/787991185427922945/1068083396863070248/history.png","You can see your transaction history in this app."))
+//        notifications.add(Notification("Transaction Success","https://media.discordapp.net/attachments/787991185427922945/1068084096758198382/tick.png" , "We have successfully received your payment for ..."))
+//        notifications.add(Notification("Transaction Failed", "https://media.discordapp.net/attachments/787991185427922945/1068083396544311306/close.png","We cannot proceed your transaction now. Please try again later."))
+//        notifications.add(Notification("Transaction Report", "https://media.discordapp.net/attachments/787991185427922945/1068083396863070248/history.png","You can see your transaction history in this app."))
+//        val gson = Gson()
+//        val json = gson.toJson(notifications)
+//        val sharedPref = getSharedPreferences("notifications_list", Context.MODE_PRIVATE)
+//        val editor = sharedPref.edit()
+//        editor.clear()
+//        editor.putString("notifications", json)
+//        editor.apply()
 
 
         //add test transactions
@@ -63,11 +93,23 @@ class MainActivity : AppCompatActivity() {
 //        transactions.add(Transaction(4, "Transaction 4",500.00 , "https://cdn.discordapp.com/emojis/949150188197531648.webp?size=128&quality=lossless"))
 //        transactions.add(Transaction(5, "Transaction 5", 1000.00,"https://cdn.discordapp.com/emojis/948911767310975037.webp?size=128&quality=lossless"))
 //        transactions.add(Transaction(6, "Transaction 6", 1500.00,"https://cdn.discordapp.com/emojis/948911453321179137.webp?size=128&quality=lossless"))
+//        transactions.add(Transaction(1, "Transaction 1",500.00 , "https://cdn.discordapp.com/emojis/949150188197531648.webp?size=128&quality=lossless"))
+//        transactions.add(Transaction(2, "Transaction 2", 1000.00,"https://cdn.discordapp.com/emojis/948911767310975037.webp?size=128&quality=lossless"))
+//        transactions.add(Transaction(3, "Transaction 3", 1500.00,"https://cdn.discordapp.com/emojis/948911453321179137.webp?size=128&quality=lossless"))
+//        transactions.add(Transaction(4, "Transaction 4",500.00 , "https://cdn.discordapp.com/emojis/949150188197531648.webp?size=128&quality=lossless"))
+//        transactions.add(Transaction(5, "Transaction 5", 1000.00,"https://cdn.discordapp.com/emojis/948911767310975037.webp?size=128&quality=lossless"))
+//        transactions.add(Transaction(6, "Transaction 6", 1500.00,"https://cdn.discordapp.com/emojis/948911453321179137.webp?size=128&quality=lossless"))
+//        transactions.add(Transaction(1, "Transaction 1",500.00 , "https://cdn.discordapp.com/emojis/949150188197531648.webp?size=128&quality=lossless"))
+//        transactions.add(Transaction(2, "Transaction 2", 1000.00,"https://cdn.discordapp.com/emojis/948911767310975037.webp?size=128&quality=lossless"))
+//        transactions.add(Transaction(3, "Transaction 3", 1500.00,"https://cdn.discordapp.com/emojis/948911453321179137.webp?size=128&quality=lossless"))
+//        transactions.add(Transaction(4, "Transaction 4",500.00 , "https://cdn.discordapp.com/emojis/949150188197531648.webp?size=128&quality=lossless"))
+//        transactions.add(Transaction(5, "Transaction 5", 1000.00,"https://cdn.discordapp.com/emojis/948911767310975037.webp?size=128&quality=lossless"))
+//        transactions.add(Transaction(6, "Transaction 6", 1500.00,"https://cdn.discordapp.com/emojis/948911453321179137.webp?size=128&quality=lossless"))
 //        val gson = Gson()
 //        val json = gson.toJson(transactions)
 //        val sharedPref = getSharedPreferences("transactions_list", Context.MODE_PRIVATE)
 //        val editor = sharedPref.edit()
-//        editor.clear()
+//        //editor.clear()
 //        editor.putString("transactions", json)
 //        editor.apply()
 
